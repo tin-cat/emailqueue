f689d92026f14d4a918477c3e367b127f8d7045e
Loren <lorenzo@tin.cat>
2020-01-09 21:01:42 +0000

    WIP

--- api/index.php
+++ api/index.php
@@ -6,10 +6,14 @@
 	*/
 
 	namespace Emailqueue;
+
+    include_once dirname(__FILE__)."/../config/application.config.inc.php"; // Include emailqueue configuration.
+    include_once dirname(__FILE__)."/../config/db.config.inc.php"; // Include Emailqueue's database connection configuration.
+    include_once dirname(__FILE__)."/../scripts/emailqueue_inject.class.php"; // Include Emailqueue's emailqueue_inject class.
+    
+    $emailqueue_inject = new Emailqueue\emailqueue_inject(EMAILQUEUE_DB_HOST, EMAILQUEUE_DB_UID, EMAILQUEUE_DB_PWD, EMAILQUEUE_DB_DATABASE); // Creates an emailqueue_inject object. Needs the database connection information.
 	
-	include_once dirname(__FILE__)."/../common.inc.php";
-	
-	$q = json_decode($utils->getglobal("q"), true);
+	$q = json_decode($_POST["q"], true);
 	if (is_null($q))
 		apiResult(false, "Can't decode query");
 
@@ -34,9 +38,13 @@
 			apiResult(false, "Key \"to\" is required");
 		if (!$message["subject"])
 			apiResult(false, "Key \"subject\" is required");
+		
+		try {
+			$result = $emailqueue_inject->inject($message);
+		} catch (Exception $e) {
+			apiResult(false, $e->getMessage());
+		}
 	}
-	
-	$db->disconnect();
 
 	apiResult(true);
 
