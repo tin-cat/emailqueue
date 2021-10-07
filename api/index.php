2cb0a61b727341842282e677f60f7ed884fd1117
Loren <loren@tin.cat>
2021-10-07 12:36:38 +0200

    API query decoding now includes info on the error

--- api/index.php
+++ api/index.php
@@ -8,15 +8,15 @@
     include_once dirname(__FILE__)."/../config/application.config.inc.php"; // Include emailqueue configuration.
     include_once dirname(__FILE__)."/../config/db.config.inc.php"; // Include Emailqueue's database connection configuration.
     include_once dirname(__FILE__)."/../scripts/emailqueue_inject.class.php"; // Include Emailqueue's emailqueue_inject class.
-    
+
     $emailqueue_inject = new Emailqueue\emailqueue_inject(EMAILQUEUE_DB_HOST, EMAILQUEUE_DB_UID, EMAILQUEUE_DB_PWD, EMAILQUEUE_DB_DATABASE); // Creates an emailqueue_inject object. Needs the database connection information.
 
 	if (!isset($_POST["q"]))
 		apiResult(false, "No query");
-	
+
 	$q = json_decode($_POST["q"], true);
 	if (is_null($q))
-		apiResult(false, "Can't decode query");
+		apiResult(false, "Can't decode query (".json_last_error().")");
 
 	if (!$q["key"] || $q["key"] == "")
 		apiResult(false, "No API key passed");
@@ -25,13 +25,13 @@
 		sleep(rand(1, 3));
 		apiResult(false, "Wrong API key");
 	}
-	
+
 	if (isset($q["message"]) && isset($q["messages"]))
 		apiResult(false, "Both message and messages have been passed, please pass only one of them");
 
 	if (isset($q["message"]))
 		$q["messages"] = [$q["message"]];
-	
+
 	foreach ($q["messages"] as $message) {
 		if (!isset($message["from"]))
 			apiResult(false, "\"from\" is required");
@@ -39,7 +39,7 @@
 			apiResult(false, "\"to\" is required");
 		if (!isset($message["subject"]))
 			apiResult(false, "\"subject\" is required");
-		
+
 		try {
 			$result = $emailqueue_inject->inject($message);
 		} catch (Exception $e) {
